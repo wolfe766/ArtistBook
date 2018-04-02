547d728220440c6ef6586e9e3d3b4a6dfe72a137
Brandon Brown <brown.5562@osu.edu>
2018-04-02 04:18:35 -0400

    Modified regex

--- ArtistBook/app/models/business_request.rb
+++ ArtistBook/app/models/business_request.rb
@@ -9,7 +9,7 @@ class BusinessRequest < ApplicationRecord
 
   # Attribute Validation
   validates :location, presence: true, length: {maximum: 250 }
-  validates :pay, presence: true, format: /(\d+)(\.|,)?\d{0,2}?\z/
+  validates :pay, presence: true, format: /\d*(\.)\d\d/
   validates :band_decision, default: false
 
 end
