c1dceaf4b34473f7a47afbf6205f7dac5ea0ff96
Brandon Brown <brown.5562@osu.edu>
2018-04-02 02:51:53 -0400

    Added validations for business request attributes

--- ArtistBook/app/models/business_request.rb
+++ ArtistBook/app/models/business_request.rb
@@ -1,2 +1,15 @@
+=begin
+  CREATED: Brandon Brown 04/07/2018
+    - Model for business requests
+=end
 class BusinessRequest < ApplicationRecord
+
+  # Table Associations
+  belongs_to :band
+
+  # Attribute Validation
+  validates :location, presence: true, length: {maximum: 250 }
+  validates :pay, presence: true, format: /(\d+)(\.|,)?\d{0,2}?\z/
+  validates :band_decision, default: 0
+
 end
