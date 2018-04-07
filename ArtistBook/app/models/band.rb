d4f162dcde79811ee9f35ff43bd53efdf94f7740
David Levine <levine.1726@osu.edu>
2018-04-07 14:20:57 -0400

    Migrated Band model into Schema.rb

--- ArtistBook/app/models/band.rb
+++ ArtistBook/app/models/band.rb
@@ -4,15 +4,13 @@ class Band < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable
 
-  attr_accessible :band_name, :genre, :phone, :address, :photo_name
-
   #attribute validations
   validates :band_name, presence: true, length: {maximum: 250}
   validates :genre, length: {maximum: 100}
   validates :phone, format: /\d\d\d-\d\d\d-\d\d\d\d/
 
   #Associations
-  has_many :posts, through :responds
+  has_many :posts, through: :responds
   has_many :business_requests
 
 end
