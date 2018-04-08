b571ffe2c95e88571f95a9cc6cda15ca995fc86f
Alec Maier <maier.144@osu.edu>
2018-04-07 22:14:54 -0400

    Added model for business, fixed typo in before_action :configure_permitted_parameters

--- ArtistBook/app/controllers/application_controller.rb
+++ ArtistBook/app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
+# CREATED: David Levine 4/7/2018
+# MODIFIED: Alec Maier 4/7/2018
+#   - added permitted parameters for business
+# Description: Main application controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parametrs, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   #setup our additional parameters for our models through
   #sanitization.
 
@@ -9,6 +13,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:band_name, :genre, :phone, :address, :photo_name])
   	devise_parameter_sanitizer.permit(:account_update, keys: [:band_name, :genre, :phone, :address, :photo_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:business_name, :phone, :address, :photo_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:business_name, :phone, :address, :photo_name])
   end
 
 end
---
+++ ArtistBook/app/models/business.rb
@@ -0,0 +1,18 @@
+# CREATED: Alec Maier 4/7/2018
+# Description: Model for a business in the database
+
+class Business < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :lockable #lockable added
+
+  # Business attribute validations
+  validates :business_name, presence: true, length: {maximum: 50}
+  validates :address, presence: true, length: {maximum: 100}
+  validates :phone, format: /\d\d\d-\d\d\d-\d\d\d\d/
+
+  # Business associations
+  has_many :business_requests
+  has_many :posts
+end
