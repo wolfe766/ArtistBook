991d74cf736db281173ce483587983d7c3b5631f
Henry Karagory <henrytkaragory@gmail.com>
2018-04-08 21:32:58 -0400

    Fixed the only: part of the has for businesses and band

--- ArtistBook/config/routes.rb
+++ ArtistBook/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :bands
   
   resources :posts
-  resources :businesses, :only [:show]
-  resources :band, :only [:show]
+  resources :businesses, only: [:show]
+  resources :band, only: [:show]
   resources :responses
   resources :business_requests
 end
