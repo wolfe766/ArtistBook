302845965e2ba42713df98bfafba335d88bb3609
Henry Karagory <henrytkaragory@gmail.com>
2018-04-08 20:54:42 -0400

    Configured necessary routes corresponding to models in the database

--- ArtistBook/config/routes.rb
+++ ArtistBook/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :businesses
   devise_for :bands
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :posts
+  resources :businesses, :only [:show]
+  resources :band, :only [:show]
+  resources :responses
+  resources :business_requests
 end
