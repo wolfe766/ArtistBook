b60bc531dfa5e1ea291dbb3557a857c95e46e280
Henry Karagory <henrytkaragory@gmail.com>
2018-04-09 16:55:42 -0400

    Removed unnecessary query

--- ArtistBook/app/controllers/businesses_controller.rb
+++ ArtistBook/app/controllers/businesses_controller.rb
@@ -10,7 +10,6 @@ class BusinessesController < ApplicationController
   # GET /businesses/1
   # GET /businesses/1.json
   def show
-    @business = Business.find(params[:id])
   end
 
   # GET /businesses/new
