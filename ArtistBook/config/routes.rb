d3550bed6b70fbece639111b552bf77e3bd51c52
Henry Karagory <henrytkaragory@gmail.com>
2018-04-09 16:53:44 -0400

    Added scoped routes for authenticated and unathenticated businesses and bands

--- ArtistBook/config/routes.rb
+++ ArtistBook/config/routes.rb
@@ -1,11 +1,29 @@
 Rails.application.routes.draw do
   devise_for :businesses
   devise_for :bands
-  
   resources :posts
   resources :businesses
-  resources :bands, only: [:show]
+  resources :bands
   resources :responses
   resources :business_requests
   resources :searches
+
+  
+  devise_scope :band do 
+    unauthenticated :band do
+      root to: 'devise/sessions#new', as: 'unathenticated_band_root'
+    end
+    authenticated :band do
+      root to: 'posts#index', as: 'athenticated_band_root'
+    end
+  end
+
+	devise_scope :business do
+  	unauthenticated :business do
+      root 'devise/sessions#new', as: 'unathenticated_business_root'
+    end
+    authenticated :business do
+      root 'businesses#index', as: 'athenticated_business_root'
+    end
+	end
 end
