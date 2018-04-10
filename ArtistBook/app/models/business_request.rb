ccea9f128622937576cdef4d3a97bc85bdd3fa36
Henry Karagory <henrytkaragory@gmail.com>
2018-04-10 11:32:10 -0400

    Added business as foreign key relation

--- ArtistBook/app/models/business_request.rb
+++ ArtistBook/app/models/business_request.rb
@@ -1,15 +1,13 @@
 =begin
-  CREATED: Brandon Brown 04/07/2018
-    - Model for business requests
+   CREATED: Brandon Brown 04/07/2018
+   Model for business requests
 =end
 class BusinessRequest < ApplicationRecord
-
-  # Table Associations
+  belongs_to :business
   belongs_to :band
 
   # Attribute Validation
   validates :location, presence: true, length: {maximum: 250 }
   validates :pay, presence: true
   validates :band_decision, default: false
-
 end
