998bf42b60bdff56c5dd8632fd6eaa174fcd0f49
Brandon Brown <brown.5562@osu.edu>
2018-04-13 01:01:48 -0400

    Added test cases for business profile and migrated

--- ArtistBook/test/models/business_profile_test.rb
+++ ArtistBook/test/models/business_profile_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class BusinessProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  # Test setup
+  def setup
+    # Test Business Profile 1
+    @profile = BusinessProfile.new
+    @profile.bio = "a" * 1000
+    @profile.spotify_link = "spotify_link.com"
+  end
+
+  # Test business profile one
+  test "Profile Bio - Valid" do 
+    assert_equal @profile.bio, 'a' * 1000
+  end 
+
+  test "Profile Spotify Link - Valid" do
+    assert_equal @profile.spotify_link, "spotify_link.com"
+  end 
 end
