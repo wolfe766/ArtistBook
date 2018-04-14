9c6bb4c5fb21989f2a973a98c7b2d31712ea6ba1
Henry Karagory <henrytkaragory@gmail.com>
2018-04-14 11:32:51 -0400

    Added business accept/decline functionality to show posts page

--- ArtistBook/app/assets/javascripts/post.js
+++ ArtistBook/app/assets/javascripts/post.js
@@ -1,8 +1,29 @@
 /*
+	Created: Henry Karagory 04/12/2018
+		- This file contains javascript for pages related to posts.
+
 	MODIFIED: 04/13/2018 Henry Karagory and David Levine
 		-Updated postId and bandId so that they no longer would
 		access parent to get the html
 */
+
+/*
+	CREATED: Henry Karagory 04/14/2018 
+*/
+function updateResponse(responseId, bandId, postId, bus_decision){
+	$.ajax({
+		  type: "PATCH",
+		  url: '/responses/' + responseId + '.json',
+		  data: {response: {band_id: bandId, post_id: postId, bus_decision: bus_decision}},
+		  success: function(data){
+		  	if(data.status == "decided"){
+		  		alert("You already made a decision for that post!");
+		  	}
+		  },
+		  dataType: 'json'
+	});
+}
+
 
 $( document ).ready(function() {
 	$.ajaxSetup({
@@ -14,7 +35,6 @@ $( document ).ready(function() {
 	$('.post-apply-button').click(function(){
 		var postId = $(this).siblings('.post-partial-post-id').html();
 		var bandId = $(this).siblings('.post-partial-band-id').html();
-		console.log ("\n" + postId + " and " +bandId)
 		$.ajax({
 		  type: "POST",
 		  url: '/responses.json',
@@ -30,5 +50,55 @@ $( document ).ready(function() {
 		var parent = $(this).parent();
 		$(this).remove();
 		parent.append('<button class=\'btn btn-success\'>Applied!</button>');
+	});
+
+	// Accept a band response and decline all others on the posts/# page 
+	$('.accept-band-response-button').click(function(){
+		// Get the ids needed to update the response
+		var responseId = $(this).siblings('.band-partial-response-id').html();
+		var bandId = $(this).siblings('.band-partial-band-id').html();
+		var postId = $(this).siblings('.band-partial-post-id').html();
+		
+		// Accept the response
+		updateResponse(responseId, bandId, postId, 'accept');
+
+		// Replace the accept and decline buttons with an accepted button
+		var parent = $(this).parent();
+		$(this).siblings('.decline-band-response-button').remove();
+		$(this).remove();
+		parent.append('<button class=\'btn btn-success\'>Accepted!</button>');
+
+		// Decline all other requests on the page
+		$('.cards .decline-band-response-button').each(function(index){
+			var responseId = $(this).siblings('.band-partial-response-id').html();
+			var bandId = $(this).siblings('.band-partial-band-id').html();
+			var postId = $(this).siblings('.band-partial-post-id').html();
+
+			setTimeout(function(){
+				updateResponse(responseId, bandId, postId, 'decline');
+			}, 500*index);
+
+			var parent = $(this).parent();
+			$(this).siblings('.accept-band-response-button').remove();
+			$(this).remove();
+			parent.append('<button class=\'btn btn-danger\'>Declined!</button>');
+		});
+	});
+
+	// Decline a band response on the posts/# page
+	$('.decline-band-response-button').click(function(){
+		// Get the ids needed to update the response
+		var responseId = $(this).siblings('.band-partial-response-id').html();
+		var bandId = $(this).siblings('band-partial-band-id').html();
+		var postId = $(this).siblings('band-partial-post-id').html();
+		
+		// Decline the response
+		updateResponse(responseId, bandId, postId, 'decline');
+		
+		// Replace accept and decline buttons with a declined button
+		var parent = $(this).parent();
+		$(this).siblings('.accept-band-response-button').remove();
+		$(this).remove();
+		parent.append('<button class=\'btn btn-danger\'>Declined</button>');
 	});
 });
