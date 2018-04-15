7052497d104869aedd39628c282c0501b4224e8b
David Levine <levine.1726@osu.edu>
2018-04-14 23:38:24 -0400

    Post validations have been created

--- ArtistBook/app/models/post.rb
+++ ArtistBook/app/models/post.rb
@@ -1,5 +1,18 @@
+=begin
+	MODIFIED: David Levine 4/14/2018
+		-Added validations to the post attributes.
+
+
+=end
 class Post < ApplicationRecord
+
+	#associations
 	belongs_to :business
 	has_many :responses
 	has_many :bands, through: :responses
+
+	#validations
+	validates :date, presence: true, format: /\d\d\d\d-((0[1-9])|([1][0-2]))-((0[1-9])|(([1-2]\d)|([3][0-1])))/
+	validates :message, presence: true, length: {maximum: 250}
+	validates :pay, presence: true
 end
