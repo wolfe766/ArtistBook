613b0bb1be020a0f42a5da87dbd9fa71b3610a63
Sam Wolfe <wolfe.766@osu.edu>
2018-04-15 17:49:19 -0400

    Made changes to javascript coce and to homepage styling

--- ArtistBook/app/assets/javascripts/business_request.js
+++ ArtistBook/app/assets/javascripts/business_request.js
@@ -4,7 +4,7 @@
 */
 
 
-$( document ).ready(function() {
+$( document ).on('turbolinks:load', function() {
 	$.ajaxSetup({
 	  headers: {
 	    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
