9531275bdb05dccf2f6a7274320a45dc2cfadd35
David Levine <levine.1726@osu.edu>
2018-04-16 21:49:16 -0400

    Fixed background image disappearing after failed login

--- ArtistBook/db/schema.rb
+++ ArtistBook/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180415212524) do
 
   create_table "band_profiles", force: :cascade do |t|
